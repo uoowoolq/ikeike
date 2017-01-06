1e27e118f055e7785fded4abf3516bb58c34bd36
uoowoolq <uoowoolq@gmail.com>
2017-01-06 17:00:54 +0900

    01061700

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 <ul class="container">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <a href="<?php the_permalink(); ?>">
-      <li class="thumbnail" style="background-image: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>);">
+
+      <li>
+        <a href="<?php the_permalink(); ?>"  class="thumbnail" style="background-image: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>);">
       <!-- 投稿のタイトルとパーマリンクを表示 -->
       <h2><?php the_title(); ?></h2>
       <!-- 日時を表示 -->
       <small><?php the_time('F jS, Y'); ?></small>
       <!-- 投稿の本文をdiv内に表示 -->
       <?php the_content(); ?>
+      </a>
     </li>
-  </a>
   <?php endwhile; else: ?>
     <p>投稿はありません。</p>
   <?php endif; ?>
