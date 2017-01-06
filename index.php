1878c0502ef61af003cedf240503857df17b7ce8
uoowoolq <uoowoolq@gmail.com>
2017-01-06 17:11:14 +0900

    01061710

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
       <li>
-        <a href="<?php the_permalink(); ?>"  class="thumbnail" style="background-image: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>);">
+        <a href="<?php the_permalink(); ?>">
+          <div class="thumbnail" style="background-image: url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]; ?>);"></div>
       <!-- 投稿のタイトルとパーマリンクを表示 -->
       <h2><?php the_title(); ?></h2>
       <!-- 日時を表示 -->
