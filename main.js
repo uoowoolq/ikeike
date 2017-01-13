73914906c55c943f0111e09502d4a102814fc959
uoowoolq <uoowoolq@gmail.com>
2017-01-13 18:21:54 +0900

    170113

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 jQuery(document).ready(function(){
   jQuery('.slick').slick({
-    slidesToShow: 2,
+    slidesToShow: 3,
     dots: true,
     infinite: true,
-    autoplay: true
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+
+          slidesToShow: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
